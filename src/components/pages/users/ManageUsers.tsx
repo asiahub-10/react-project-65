ee723796ea6ebef2794273ef02b08471302ef8c0
asiahub-10 <asiarahman104@gmail.com>
2025-10-06 13:05:24 +0600

    jwt, delete

--- src/components/pages/users/ManageUsers.tsx
+++ src/components/pages/users/ManageUsers.tsx
@@ -6,6 +6,7 @@ import { baseUrl } from "../../../config";
 
 function ManageUsers() {
   const [users, setUsers] = useState<User[]>([]);
+  const [userId, setUserId] = useState<number | undefined>(0);
   useEffect(() => {
     document.title = "Manage Users";
     getUsers();
@@ -20,6 +21,25 @@ function ManageUsers() {
       console.error(err);
     })
   }
+  function handleDelete(user_id:any){
+    // alert("delete id: "+id);
+
+    // api.delete(`delete-user?id=${user_id}`)
+    api.delete(`delete-user`,{
+      params: {
+        id: user_id,
+        name: "Asia",
+      }
+    })
+    .then((res) => {
+      console.log(res.data);
+      getUsers();
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+  
   return (
     <>
     <div className="container-xxl flex-grow-1 container-p-y">
@@ -55,7 +75,7 @@ function ManageUsers() {
                                         <Link to={`/post/edit/${user.id}`} type="button" className="btn btn-icon btn-outline-primary">
                                             <span className="tf-icons bx bx-edit"></span>
                                         </Link>
-                                        <button type="button" className="btn btn-icon btn-outline-danger">
+                                        <button type="button" className="btn btn-icon btn-outline-danger" onClick={()=>setUserId(user?.id)} data-bs-toggle="modal" data-bs-target="#deleteModal">
                                             <span className="tf-icons bx bx-trash"></span>
                                         </button>
                                     </div>
@@ -65,6 +85,25 @@ function ManageUsers() {
                     }
                 </tbody>
             </table>
+        </div>
+      </div>
+    </div>
+
+    {/* Delete modal */}
+    <div className="modal" id="deleteModal" tabIndex={-1}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body text-center">
+            <span className="bx bx-trash fs-1 text-danger mb-3"></span>
+            <h5 className="text-center mb-0">Are you sure you want to delete?</h5>
+          </div>
+          <div className="modal-footer justify-content-center">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={()=>handleDelete(userId)}>Delete</button>
+          </div>
         </div>
       </div>
     </div>
